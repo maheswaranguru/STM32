3e32b3dbfa783ff613b15b7ad2803ecc7d6c07ed
mahesh <maheswaranguru@gmail.com>
2020-07-05 05:06:05 +0530

    static IP. Ping working and able to see on wireshark.

---
+++ BaseCodeGen/F7/portingDriver/Core/Src/application/application.c
@@ -0,0 +1,71 @@
+/*
+ * application.c
+ *
+ *  Created on: 22-Mar-2019
+ *      Author: gmahez
+ */
+#include "freertos.h"
+#include "task.h"
+#include "queue.h"
+
+
+#include "application.h"
+//#include "button.h"
+#include "led.h"
+#include "debugConsole.h"
+
+#include "../encoder/encoder.h"
+
+
+void debugconsoleTask(void);
+
+/* Private Constant -----------------------------------------------*/
+const sysTask_t sysTask[] =
+{
+    { (TaskFunction_t) ledTask,             "Led",              254, 0, osPriorityNormal, NULL },
+ // { (TaskFunction_t) buttonTask,          "button",           128, 0, osPriorityNormal, NULL },
+    { (TaskFunction_t) debugconsoleTask,    "debugconsole",     254, 0, osPriorityNormal, NULL },
+    { (TaskFunction_t) voltageMgrTask, 		"VoltageManager",   254, 0, osPriorityNormal, NULL }
+
+};
+
+/* Private function prototypes -----------------------------------------------*/
+
+/* Private variables ---------------------------------------------------------*/
+osThreadId defaultTaskHandle;
+osThreadId ledHandle;
+osThreadId buttonHandle;
+
+TaskHandle_t xHandle = NULL;
+
+
+void application(void)
+{
+    volatile uint8_t i = 0;
+    volatile uint8_t TaskStatus = 0;
+
+    for( i = 0; i< MAXMUM_TASK; i++)
+    {
+        if( pdPASS !=  xTaskCreate(sysTask[i].vTaskfunPtr, sysTask[i].vTaskName, sysTask[i].stacksize, sysTask[i].VTaskparaeter, sysTask[i].VtaskPriority, sysTask[i].pxCreatedTask) )
+        {
+            TaskStatus++;
+        }
+    }
+
+    if( 0 != TaskStatus )       //!< If any of the task not created successfully, freeze here !
+    {
+        while(1);
+    }
+
+
+    xMutexDebugUart = xSemaphoreCreateMutex();
+    if( NULL == xMutexDebugUart )
+    {
+        // TURN ON ERROR LED;
+    }
+
+
+    /* Start scheduler */
+    osKernelStart();
+}
+
---
+++ BaseCodeGen/F7/portingDriver/Core/Src/application/application.h
@@ -0,0 +1,41 @@
+/*
+ * application.h
+ *
+ *  Created on: 22-Mar-2019
+ *      Author: gmahez
+ */
+
+#ifndef APPLICATION_APPLICATION_H_
+#define APPLICATION_APPLICATION_H_
+
+#include "cmsis_os.h"   //!< included it to use some macro definition.
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "sysConfigEsab.h"
+
+typedef struct taskpara
+{
+	TaskFunction_t  vTaskfunPtr;
+    char vTaskName[configMAX_TASK_NAME_LEN];
+	unsigned short stacksize;
+	void * VTaskparaeter;
+	UBaseType_t VtaskPriority;
+	TaskHandle_t *pxCreatedTask;
+}sysTask_t;
+
+//!<  THIS BELOW ENUM SEQUENCE IS DIRECTLY CONNECT WITH BELOW TASK PARAMETER STRUCTURE ( sysTask ).
+//!< PLEASE TAKECARE WHILE MODIFING "sysTaskList" and sysTask
+typedef enum{
+	LED_TASK,
+	//BUTTON_TASK,
+	CONSOLE_TASK,
+	VOLTMANAGER,
+	MAXMUM_TASK
+}sysTaskList_t;
+
+
+//Extern Function
+void application( void );
+
+#endif /* APPLICATION_APPLICATION_H_ */
